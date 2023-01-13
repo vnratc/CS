19a642743f83fd0e890e0e042829fe269607c4f8
vnratc <105481726+vnratc@users.noreply.github.com>
2023-01-13 22:48:59 +0400

    Delete w7 directory

--- w7/src7/favorites/notes.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-with open("favorites.csv", "r") as file:
-    reader = csv.DictReader(file)
-    counts = {}
-    for row in reader:
-        title = row["title"]
-        counts[title] += 1
