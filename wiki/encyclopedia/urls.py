422024dabe7156d4a7bfd3073e5a6f90a84262cb
vnratc <105481726+vnratc@users.noreply.github.com>
2023-02-08 15:31:34 +0400

    folder_reorg0

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from . import views
 
-app_name = "encyclopedia"
+# if you decide to  add app_name here, then you have to refer to it in html: <a href="{% url 'encyclopedia:index' %}">Home</a>
+# app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:title>", views.)
+    path("<str:title>", views.display_contents, name="title"),
+    path("search", views.search, name="search")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,11 +1,29 @@
+from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from . import util
 
+class SearchForm(forms.Form):
+    query = forms.CharField()
 
 def index(request):
+    
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def display_contents(request, title):
+    if not util.get_entry(title):   # if util.get_entry(title) == None:
+        return render(request, 'encyclopedia/not_found.html')
+    else:
+        contents = util.get_entry(title)
+        return render(request, "encyclopedia/contents.html", {
+            "contents": contents,
+            "title": title.capitalize()
+        })
 
+def search(request):
+    
+    return render(request)
