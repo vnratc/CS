d720a270e6d277a718ea4afaa96d7a6927ab7707
vnratc <105481726+vnratc@users.noreply.github.com>
2023-02-11 07:34:28 +0400

    completed Edit Page

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("search", views.search, name="search"),    # placing "search" path turned out to be CRITICAL, since when it was below "<str:title>" path, search never got to be accessed and called.
     path("new_page", views.new_page, name="new_page"),
+    path("edit_page", views.edit_page, name="edit_page"),
+    path("save_edit", views.save_edit, name="save_edit"),
     path("<str:title>", views.display_contents, name="title")
 ]
