e2e9ff50767d859ca708a3b00944310609dbefcf
vnratc <105481726+vnratc@users.noreply.github.com>
2023-02-13 19:30:51 +0400

    wdb

--- WDB/Functions_Intro_CODE/s.js
+++ WDB/Functions_Intro_CODE/s.js
@@ -60,3 +60,18 @@
 //     return sum
 // }
 
+function returnDay(number) {
+    let week = {
+        1: 'Monday',
+        2: 'Tuesday',
+        3: 'Wednesday', 
+        4: 'Thursday',
+        5: 'Friday',
+        6: 'Saturday',
+        7: 'Sunday'
+    }
+    if (number < 1 || number > 7) {
+        return null
+    }
+    return week[number]
+}
