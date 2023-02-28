dea5f3f31050090a3081f4234b93d9b0a9354735
vnratc <105481726+vnratc@users.noreply.github.com>
2023-02-28 12:04:57 +0400

    mail, Done isolate "view_email" function. Newx make history work.

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -1,3 +1,8 @@
+
+document.querySelectorAll('.email-item').forEach(item => {
+    item.addEventListener('click', () => console.log('click'))
+})
+
 document.addEventListener('DOMContentLoaded', function () {
 
     // Use buttons to toggle between views
@@ -5,7 +10,6 @@ document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
     document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
     document.querySelector('#compose').addEventListener('click', compose_email);
-
     // By default, load the inbox
     load_mailbox('inbox');
     
@@ -81,99 +85,105 @@ async function load_mailbox(mailbox) {
             div.innerHTML = `From: ${email.sender}. Subject: ${email.subject}. Date & Time: ${email.timestamp}.`
             document.querySelector('#emails-view').append(div)
         }
+    })
+    document.querySelectorAll('.email-item').forEach(email => {
+        email.onclick = function() {
+            // console.log(email.dataset.id)
+            view_email(email.dataset.id, mailbox)
+        } 
+    })
+}
 
-        // View Email
+// View Email
+async function view_email(email_item, mailbox) {
+    const viewEmail = email_item
+    history.pushState({viewEmail: viewEmail}, "", `${viewEmail}`)
+    document.querySelector('#emails-view').style.display = 'none';
+    document.querySelector('#view-email').style.display = 'block';
+    await fetch('emails/' + email_item)
+    .then(response => response.json())
+    .then(email => {
+        // Create 5 divs
+        for (let i = 0; i < 5; i++) {
+            let new_div = document.createElement('div')
+            new_div.setAttribute('id', 'div' + i)
+            new_div.classList.add('email-content')
+            document.querySelector('#view-email').append(new_div)
+        }
+        // Populate divs with data
+        document.querySelector('#div0').innerHTML = `From: ${email.sender}`
+        document.querySelector('#div1').innerHTML = `Recipients: ${email.recipients}`
+        document.querySelector('#div2').innerHTML = `Subject: ${email.subject}`
+        document.querySelector('#div3').innerHTML = `Date & Time: ${email.timestamp}`
+        document.querySelector('#div4').innerHTML = `${email.body}`
 
-        document.querySelectorAll('.email-item').forEach(email_item => {
-            email_item.onclick = async function() {
-                document.querySelector('#emails-view').style.display = 'none';
-                document.querySelector('#view-email').style.display = 'block';
-                await fetch('emails/' + email_item.dataset.id)
-                .then(response => response.json())
-                .then(email => {
-                    // Create 5 divs
-                    for (let i = 0; i < 5; i++) {
-                        let new_div = document.createElement('div')
-                        new_div.setAttribute('id', 'div' + i)
-                        new_div.classList.add('email-content')
-                        document.querySelector('#view-email').append(new_div)
-                    }
-                    // Populate divs with data
-                    document.querySelector('#div0').innerHTML = `From: ${email.sender}`
-                    document.querySelector('#div1').innerHTML = `Recipients: ${email.recipients}`
-                    document.querySelector('#div2').innerHTML = `Subject: ${email.subject}`
-                    document.querySelector('#div3').innerHTML = `Date & Time: ${email.timestamp}`
-                    document.querySelector('#div4').innerHTML = `${email.body}`
+        // Archive and Unarchive
 
-                    // Archive and Unarchive
-
-                    if (mailbox !== 'sent') {
-                        let button = document.createElement('button')
-                        button.dataset.id = email.id
-                        document.querySelector('#view-email').append(button)
-                        button.classList.add('btn', 'btn-secondary')
-                        // Archive
-                        if (mailbox === 'inbox') {
-                            button.innerHTML = 'Archive'
-                            button.onclick = async () => {
-                                await fetch('/emails/' + button.dataset.id, {
-                                    method: 'PUT',
-                                    body: JSON.stringify({
-                                        archived: true
-                                    })
-                                })
-                                load_mailbox('inbox')
-                            }                            
-                        }
-
-                        // Unarchive
-                        else if (mailbox === 'archive') {
-                            button.innerHTML = 'Unarchive'
-                            button.onclick = async () => {
-                                await fetch('/emails/' + button.dataset.id, {
-                                    method: 'PUT',
-                                    body: JSON.stringify({
-                                        archived: false
-                                    })
-                                })
-                                load_mailbox('inbox')
-                            }
-                        }
-                    }
-
-                    // Reply
-
-                    let button_reply = document.createElement('button')
-                    button_reply.classList.add('btn', 'btn-primary')
-                    button_reply.innerHTML = 'Reply'
-                    document.querySelector('#view-email').append(button_reply)
-                    button_reply.onclick = () => {
-                        // Show compose view and hide other views
-                        document.querySelector('#emails-view').style.display = 'none';
-                        document.querySelector('#compose-view').style.display = 'block';
-                        document.querySelector('#view-email').style.display = 'none';
-
-                        // Clear out composition fields
-                        document.querySelector('#compose-recipients').value = email.sender;
-                        console.log(email.subject.slice(0, 4))
-                        if (email.subject.slice(0, 4) !== 'Re: ') {
-                            document.querySelector('#compose-subject').value = `Re: ${email.subject}`
-                        } else {
-                            document.querySelector('#compose-subject').value = email.subject
-                        }
-                        document.querySelector('#compose-body').value = `\n\nOn ${email.timestamp} ${email.sender} wrote:\n${email.body}`;
-                    }
-                })
-                // Mark as "read"
-                await fetch('emails/' + email_item.dataset.id, {
-                    method: 'PUT',
-                    body: JSON.stringify({
-                        read: true
+        if (mailbox !== 'sent') {
+            let button = document.createElement('button')
+            button.dataset.id = email.id
+            document.querySelector('#view-email').append(button)
+            button.classList.add('btn', 'btn-secondary')
+            // Archive
+            if (mailbox === 'inbox') {
+                button.innerHTML = 'Archive'
+                button.onclick = async () => {
+                    await fetch('/emails/' + button.dataset.id, {
+                        method: 'PUT',
+                        body: JSON.stringify({
+                            archived: true
+                        })
                     })
-                })
+                    load_mailbox('inbox')
+                }                            
             }
+
+            // Unarchive
+            else if (mailbox === 'archive') {
+                button.innerHTML = 'Unarchive'
+                button.onclick = async () => {
+                    await fetch('/emails/' + button.dataset.id, {
+                        method: 'PUT',
+                        body: JSON.stringify({
+                            archived: false
+                        })
+                    })
+                    load_mailbox('inbox')
+                }
+            }
+        }
+
+        // Reply
+
+        let button_reply = document.createElement('button')
+        button_reply.classList.add('btn', 'btn-primary')
+        button_reply.innerHTML = 'Reply'
+        document.querySelector('#view-email').append(button_reply)
+        button_reply.onclick = () => {
+            // Show compose view and hide other views
+            document.querySelector('#emails-view').style.display = 'none';
+            document.querySelector('#compose-view').style.display = 'block';
+            document.querySelector('#view-email').style.display = 'none';
+
+            // Clear out composition fields
+            document.querySelector('#compose-recipients').value = email.sender;
+            console.log(email.subject.slice(0, 4))
+            if (email.subject.slice(0, 4) !== 'Re: ') {
+                document.querySelector('#compose-subject').value = `Re: ${email.subject}`
+            } else {
+                document.querySelector('#compose-subject').value = email.subject
+            }
+            document.querySelector('#compose-body').value = `\n\nOn ${email.timestamp} ${email.sender} wrote:\n${email.body}`;
+        }
+    })
+    // Mark as "read"
+    await fetch('emails/' + email_item, {
+        method: 'PUT',
+        body: JSON.stringify({
+            read: true
         })
     })
-
 }
+
+
 
