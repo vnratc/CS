e4a7d8cb2a685b4ef98da50145fb39722c0c1ef5
vnratc <105481726+vnratc@users.noreply.github.com>
2023-03-02 14:57:58 +0400

    test github actions

--- CS50W/w7/prime.py
+++ CS50W/w7/prime.py
@@ -1,9 +1,9 @@
 import math
    
-        rime(n):
+def is_prime(n):
     if n < 2:
         return False
-    for i in range(2, int(math.sqrt(n))):
+    for i in range(2, int(math.sqrt(n)) + 1):
         if n % i == 0:
             return False
     return True
--- CS50W/w7/tests.py
+++ CS50W/w7/tests.py
@@ -3,7 +3,6 @@ import pathlib
 import unittest
 
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 
 
 def file_uri(filename):
@@ -21,22 +20,22 @@ class WebpageTests(unittest.TestCase):
 
     def test_increase(self):
         driver.get(file_uri("counter.html"))
-        increase = driver.find_element("id", "increase")
+        increase = driver.find_element_by_id("increase")
         increase.click()
-        self.assertEqual(driver.find_element(By.TAG_NAME,"h1").text, "1")
+        self.assertEqual(driver.find_element_by_tag_name("h1").text, "1")
 
     def test_decrease(self):
         driver.get(file_uri("counter.html"))
-        decrease = driver.find_element("id", "decrease")
+        decrease = driver.find_element_by_id("decrease")
         decrease.click()
-        self.assertEqual(driver.find_element(By.TAG_NAME,"h1").text, "-1")
+        self.assertEqual(driver.find_element_by_tag_name("h1").text, "-1")
 
     def test_multiple_increase(self):
         driver.get(file_uri("counter.html"))
-        increase = driver.find_element("id", "increase")
+        increase = driver.find_element_by_id("increase")
         for i in range(3):
             increase.click()
-        self.assertEqual(driver.find_element(By.TAG_NAME,"h1").text, "3")
+        self.assertEqual(driver.find_element_by_tag_name("h1").text, "3")
 
 
 if __name__ == "__main__":
