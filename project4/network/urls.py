66ca24b09215a515506d85d628aeef66b37d05f0
vnratc <105481726+vnratc@users.noreply.github.com>
2023-03-04 14:25:23 +0400

    Done Following. Next Pagination

--- project4/network/urls.py
+++ project4/network/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
     path("new_post", views.new_post, name="new_post"),
+    path("following", views.following, name="following"),
     path("<int:user_id>", views.profile, name="profile"),
     path("<int:user_id>follow", views.follow, name="follow"),
     path("<int:user_id>unfollow", views.unfollow, name="unfollow")
