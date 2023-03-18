18f298451361a0c4d14d191807dd64ce1c7c2d20
vnratc <105481726+vnratc@users.noreply.github.com>
2023-03-18 21:49:56 +0400

    wdb 25

--- WDB/24 DOM_Intro_CODE/Pokemon/app.js
+++ WDB/24 DOM_Intro_CODE/Pokemon/app.js
@@ -1,19 +1,35 @@
 // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
 
-const container = document.querySelector('#container');
+// const container = document.querySelector('#container');
+// const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+
+
+// for (let i = 1; i <= 151; i++) {
+//     const pokemon = document.createElement('div');
+//     pokemon.classList.add('pokemon');
+//     const label = document.createElement('span');
+//     label.innerText = `#${i}`;
+//     const newImg = document.createElement('img');
+//     newImg.src = `${baseURL}${i}.png`
+
+
+//     pokemon.appendChild(newImg);
+//     pokemon.appendChild(label);
+//     container.appendChild(pokemon)
+// }
+
+
+// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+
+const container = document.querySelector('#container')
 const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
-
-
-for (let i = 1; i <= 151; i++) {
-    const pokemon = document.createElement('div');
-    pokemon.classList.add('pokemon');
-    const label = document.createElement('span');
-    label.innerText = `#${i}`;
-    const newImg = document.createElement('img');
-    newImg.src = `${baseURL}${i}.png`
-
-
-    pokemon.appendChild(newImg);
-    pokemon.appendChild(label);
-    container.appendChild(pokemon)
+for (i = 1; i < 151; i++) {
+    let image = document.createElement('img')
+    image.src = `${baseURL}${i}.png`
+    container.append(image)
+    let number = document.createElement('h5')
+    number.style.width = '96px'
+    number.style.textAlign = 'center'
+    number.innerText = `${i}`
+    image.insertAdjacentElement("afterend" ,number)
 }
--- WDB/24 DOM_Intro_CODE/Selecting/app.js
+++ WDB/24 DOM_Intro_CODE/Selecting/app.js
@@ -1,18 +1,84 @@
-const allImages = document.getElementsByTagName('img');
+const allImages = document.getElementsByTagName('img')
 
-for (let img of allImages) {
-    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg'
+// for (img of allImages) {
+//     img.src = 'https://images.unsplash.com/photo-1563281577-a7be47e20db9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80'
+// }
+
+const squareImages = document.getElementsByClassName('square')
+
+const allLinks = document.querySelectorAll('a')
+// for (let link of allLinks) {
+//     link.innerText = 'HEEEEEY!!! I AM A LINK!!!   FROM innerText'
+// }
+// document.querySelector('h1').innerText = '<i>asdkfjsadlf</i>'
+
+// document.querySelector('h1')
+
+// document.querySelector('#banner')
+
+
+for (let link of allLinks) {
+    link.style.color = '#7b7fdd'
+    link.style.textDecorationColor = 'magenta'
+    link.style.textDecorationStyle = 'wavy'
 }
 
+// const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
-const squareImages = document.getElementsByClassName('square');
+// const spans = document.querySelectorAll('span')
+// for (let i = 0; i < spans.length; i++) {
+//     spans[i].style.color = colors[i]
+// }
 
-for (let img of squareImages) {
-    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg';
-}
 
-const links = document.querySelectorAll('p a');
 
-for (let link of links) {
-    console.log(link.href)
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const allImages = document.getElementsByTagName('img');
+
+// for (let img of allImages) {
+//     img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg'
+// }
+
+
+// const squareImages = document.getElementsByClassName('square');
+
+// for (let img of squareImages) {
+//     img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg';
+// }
+
+// const links = document.querySelectorAll('p a');
+
+// for (let link of links) {
+//     console.log(link.href)
+// }
