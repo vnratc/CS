b2990ef7b73dc93e94e6ca203e843b6423ee7b3e
vnratc <105481726+vnratc@users.noreply.github.com>
2023-03-20 12:23:23 +0400

    wdb 25-26

--- WDB/25 DOM_Events_CODE/ChangeAndInputEvents/app.js
+++ WDB/25 DOM_Events_CODE/ChangeAndInputEvents/app.js
@@ -1,10 +1,23 @@
-const input = document.querySelector('input');
-const h1 = document.querySelector('h1');
+// const input = document.querySelector('input');
+// const h1 = document.querySelector('h1');
 
-// input.addEventListener('change', function (e) {
-//     console.log("CASKDJASKJHD")
+// // input.addEventListener('change', function (e) {
+// //     console.log("CASKDJASKJHD")
+// // })
+
+// input.addEventListener('input', function (e) {
+//     h1.innerText = input.value;
 // })
 
-input.addEventListener('input', function (e) {
-    h1.innerText = input.value;
+
+const input = document.querySelector('input')
+const h1 = document.querySelector('h1')
+
+input.addEventListener('change', function(e) {
+    console.dir(e)
+    
+})
+input.addEventListener('input', e => {
+    console.dir(e)
+    h1.innerText = input.value
 })
--- WDB/25 DOM_Events_CODE/FormEvents/app.js
+++ WDB/25 DOM_Events_CODE/FormEvents/app.js
@@ -1,25 +1,39 @@
 
 
-const tweetForm = document.querySelector('#tweetForm');
-const tweetsContainer = document.querySelector('#tweets');
-tweetForm.addEventListener('submit', function (e) {
-    e.preventDefault();
+// const tweetForm = document.querySelector('#tweetForm');
+// const tweetsContainer = document.querySelector('#tweets');
+// tweetForm.addEventListener('submit', function (e) {
+//     e.preventDefault();
 
-    // const usernameInput = document.querySelectorAll('input')[0];
-    // const tweetInput = document.querySelectorAll('input')[1];
-    const usernameInput = tweetForm.elements.username;
-    const tweetInput = tweetForm.elements.tweet;
-    addTweet(usernameInput.value, tweetInput.value)
-    usernameInput.value = '';
-    tweetInput.value = '';
-});
+//     // const usernameInput = document.querySelectorAll('input')[0];
+//     // const tweetInput = document.querySelectorAll('input')[1];
+//     const usernameInput = tweetForm.elements.username;
+//     const tweetInput = tweetForm.elements.tweet;
+//     addTweet(usernameInput.value, tweetInput.value)
+//     usernameInput.value = '';
+//     tweetInput.value = '';
+// });
 
-const addTweet = (username, tweet) => {
-    const newTweet = document.createElement('li');
-    const bTag = document.createElement('b');
-    bTag.append(username)
-    newTweet.append(bTag);
-    newTweet.append(`- ${tweet}`)
-    tweetsContainer.append(newTweet);
-}
+// const addTweet = (username, tweet) => {
+//     const newTweet = document.createElement('li');
+//     const bTag = document.createElement('b');
+//     bTag.append(username)
+//     newTweet.append(bTag);
+//     newTweet.append(`- ${tweet}`)
+//     tweetsContainer.append(newTweet);
+// }
 
+
+const form = document.querySelector('#shelterForm')
+console.dir(form)
+form.addEventListener('submit', function(e) {
+    console.log('SUBMITTED')
+    e.preventDefault()
+    const input = document.querySelector('input')
+    let text = input.value
+    let li = document.createElement('li')
+    li.innerText = text
+    const ul = document.querySelector('#cats')
+    ul.append(li)
+    input.value = ''
+})
