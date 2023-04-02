6602f0ef9eef53574dc147f0ecc0ddbb4810969e
vnratc <105481726+vnratc@users.noreply.github.com>
2023-04-02 14:29:25 +0400

    wdb 32 modules

---
+++ WDB/32 Modules_And_NPM_CODE/m.js
@@ -0,0 +1,28 @@
+const add = (x, y) => x + y;
+
+const PI = 3.14159;
+
+const square = x => x * x;
+
+
+// module.exports.add = (x, y) => x + y;
+
+// module.exports.PI = 3.14159;
+
+// module.exports.square = x => x * x;
+
+exports.square = square;
+exports.PI = PI
+
+// const math = {
+//     add: add,
+//     PI: PI,
+//     square: square
+// }
+
+// module.exports = math;
+// module.exports.add = add;
+// module.exports.PI = PI;
+// module.exports.square = square;
+
+
---
+++ WDB/32 Modules_And_NPM_CODE/script.js
@@ -0,0 +1,12 @@
+// const m = require("./m")
+// console.log(m.PI)
+
+// console.log(m.square(13))
+
+
+const { PI, square } = require("./m")
+console.log(PI)
+
+console.log(square(12))
+
+
