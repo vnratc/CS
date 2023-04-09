3ab2d0d7e05d7954525b3005c038ded616bad49c
vnratc <105481726+vnratc@users.noreply.github.com>
2023-04-09 16:15:51 +0400

    wdb 37

---
+++ WDB/37 MongooseBasics/mongoose/index.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+mongoose.connect('mongodb://127.0.0.1:27017/movieApp')
+    .then(() => {console.log("Connection open.")})
+    .catch(e => {
+        console.log("Error!")
+        console.log(e)
+    })
+
+
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String
+})
+
+
+const Movie = mongoose.model("Movie", movieSchema);
+const amadeus = new Movie({ title: "Amadeus", year: 1986, score: 9.2, rating: "R" })
+// amadeus.save() 
