864e0f7eec63dd251dc2c3e40171183ca96ee963
vnratc <105481726+vnratc@users.noreply.github.com>
2023-04-11 19:25:00 +0400

    wdb40 Mongoose with Express

--- WDB/40 Mongoose_With_Express_CODE/vnratc/seeds.js
+++ WDB/40 Mongoose_With_Express_CODE/vnratc/seeds.js
@@ -24,38 +24,38 @@ async function main() {
 //     })
 
 
-const seedProducts = [
-    {
-        name: "Fairy Eggplant",
-        price: 1.00,
-        category: "vegetable"
-    },
-    {
-        name: "Organic Goddess Melon",
-        price: 4.99,
-        category: "fruit"
-    },
-    {
-        name: "Organic Mini Seedless Watermelon",
-        price: 3.99,
-        category: "fruit"
-    },
-    {
-        name: "Organic Celery",
-        price: 1.50,
-        category: "vegetable"
-    },
-    {
-        name: "Chocolate Whole Milk",
-        price: 2.69,
-        category: "dairy"
-    }
-];
+// const seedProducts = [
+//     {
+//         name: "Fairy Eggplant",
+//         price: 1.00,
+//         category: "vegetable"
+//     },
+//     {
+//         name: "Organic Goddess Melon",
+//         price: 4.99,
+//         category: "fruit"
+//     },
+//     {
+//         name: "Organic Mini Seedless Watermelon",
+//         price: 3.99,
+//         category: "fruit"
+//     },
+//     {
+//         name: "Organic Celery",
+//         price: 1.50,
+//         category: "vegetable"
+//     },
+//     {
+//         name: "Chocolate Whole Milk",
+//         price: 2.69,
+//         category: "dairy"
+//     }
+// ];
 
-Product.insertMany(seedProducts)
-.then(res => {
-    console.log(res)
-})
-.catch(e => {
-    console.log(e)
-})
+// Product.insertMany(seedProducts)
+// .then(res => {
+//     console.log(res)
+// })
+// .catch(e => {
+//     console.log(e)
+// })
