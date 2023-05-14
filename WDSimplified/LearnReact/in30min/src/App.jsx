f2a2b4fb086f96c077c27c7cab273d1a644ba14c
vnratc <105481726+vnratc@users.noreply.github.com>
2023-05-14 10:50:02 +0400

    figured out displaying completed ratio

--- WDSimplified/LearnReact/in30min/src/App.jsx
+++ WDSimplified/LearnReact/in30min/src/App.jsx
@@ -3,10 +3,10 @@ import './App.css'
 
 function App() {
   const [todosByUser, setTodosByUser] = useState([])
-  const [loading, setLoading] = useState({btnDisabled: false, preloader: ""})
+  const [loading, setLoading] = useState({ btnDisabled: false, preloader: "" })
 
   async function fetchItems(e) {
-    setLoading({btnDisabled: true, preloader: "preloader"})
+    setLoading({ btnDisabled: true, preloader: "preloader" })
     e.preventDefault()
     await fetch("https://jsonplaceholder.typicode.com/todos")
       .then(res => res.json())
@@ -18,39 +18,38 @@ function App() {
           return groupedUsers
         }, []).slice(1)
 
-        console.log(groupedByUser)
+        // console.log(groupedByUser)
 
-        // const groupedWithCompleted = groupedByUser.map(user => {
-        //   const completed = user.filter(todo => todo.completed).length
-        //   const notCompleted = user.filter(todo => !todo.completed).length
-        //   // console.log(completed, notCompleted)
-        //   return [...user, {completed, notCompleted}]
-        // });
+        const groupedWithCompleted = groupedByUser.map(user => {
+          const completed = user.filter(todo => todo.completed).length
+          const notCompleted = user.filter(todo => !todo.completed).length
+          // console.log(completed, notCompleted)
+          return {data: [...user], ratio: { completed, notCompleted }}
+        });
 
-        // console.log(groupedWithCompleted)
+        console.log(groupedWithCompleted)
 
-        setTodosByUser(groupedByUser)
+        setTodosByUser(groupedWithCompleted)
       })
-    setLoading({btnDisabled: false, preloader: ""})
+    setLoading({ btnDisabled: false, preloader: "" })
   }
 
 
   let todoItems = []
-  todosByUser.map(user => {
-    todoItems = user.map(todo => 
-      <li key={todo.id}>
-        {todo.title}, {String(todo.completed)}
-      </li>
-      )
-  }) 
-
-
-  const listItems = todosByUser.map((user, index) =>
-    <li key={`user${user[index].userId}`}>
-      User {user[index].userId}
-        <ul>{todoItems}</ul>
+  const userItems = todosByUser.map((user, index) => {
+    // console.log(user)
+    return (
+    <li key={`user${user.data[index].userId}`}>
+      User {user.data[index].userId}, {user.ratio.completed} / {user.ratio.notCompleted}
+      <ul>{todoItems = user.data.map(todo => {
+        return (
+          <li key={todo.id}>
+            {todo.title}, {String(todo.completed)}
+          </li>
+        )
+      })}</ul>
     </li>
-  )
+    )})
 
 
   return (
@@ -60,7 +59,7 @@ function App() {
         <div className={loading.preloader}></div>
       </form>
 
-      <ul>{listItems}</ul>
+      <ul>{userItems}</ul>
     </>
   )
 }
